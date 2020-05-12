66c89900f3b430436bbe52d85db7315f01662b13
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-11 19:33:11 -0500

    working on sw

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-console.log("service worker hola");
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("style.css")) {
+    event.respondWith(fetch(null));
+  } else {
+    event.respondWith(fetch(event.request));
+  }
+});
