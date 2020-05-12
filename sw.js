9f82cf29fc9a6ebcdd3b25902e211ee31b76135d
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-11 19:42:07 -0500

    fetch on sw

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.includes("style.css")) {
-    event.respondWith(fetch(null));
-  } else {
-    event.respondWith(fetch(event.request));
+  if (event.request.url.includes(".jpg")) {
+    console.log(event.request.url);
+
+    // let fotoReq = fetch("img/main.jpg");
+    // let fotoReq = fetch(event.request.url);
+    // let fotoReq = fetch(event.request);
+
+    event.respondWith(fotoReq);
   }
 });
